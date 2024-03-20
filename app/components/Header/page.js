c8c63227445f5385980c39fd22ce73e5aac334b7
[saroj adhikari] <sawroz.adhikari@gmail.com>
2024-03-20 10:34:55 +0545

    adding hamburger menu

--- app/components/Header/page.js
+++ app/components/Header/page.js
@@ -1,53 +1,52 @@
-'use client'
+"use client";
 import React from "react";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Page = () => {
   const [click, setClick] = useState(false);
-  const hamburgerMenu = () =>{
+  const hamburgerMenu = () => {
     setClick(!click);
     console.log("Clicked");
-  }
+  };
   return (
-    <header className="flex items-center justify-between bg-slate-400 py-4 px-6">
-      <div className="flex items-center">
-        <img className="h-10 w-auto mr-4" src="logo.png" alt="logo" />
-        <nav className="hidden sm:block">
-          <ul className="flex">
-            <li className="mr-4">
-              <a href="#" className="text-white hover:text-gray-300">
-                Home
-              </a>
-            </li>
-            <li className="mr-4">
-              <a href="#" className="text-white hover:text-gray-300">
-                Skills
-              </a>
-            </li>
-            <li className="mr-4">
-              <a href="#" className="text-white hover:text-gray-300">
-                Education
-              </a>
-            </li>
-            <li className="mr-4">
-              <a href="#" className="text-white hover:text-gray-300">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-white hover:text-gray-300">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <header className="flex items-center justify-between bg-red-300 py-4 px-6">
+      <img className="h-12 w-13 mr-4" src="logo.png" alt="logo" />
+
+      <nav className="hidden sm:block items-end justify-end">
+        <ul className="flex justify-end items-end">
+          <li className="mr-4">
+            <a href="#" className="text-white hover:text-gray-300">
+              Home
+            </a>
+          </li>
+          <li className="mr-4">
+            <a href="#" className="text-white hover:text-gray-300">
+              Skills
+            </a>
+          </li>
+          <li className="mr-4">
+            <a href="#" className="text-white hover:text-gray-300">
+              Education
+            </a>
+          </li>
+          <li className="mr-4">
+            <a href="#" className="text-white hover:text-gray-300">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white hover:text-gray-300">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
       {/* Hamburger Menu for small screens */}
-      <div className="block sm:hidden">
-        <button className="text-white focus:outline-none" onClick={hamburgerMenu}>
-        <FontAwesomeIcon icon='fa-soild fa-bars' />
-        </button>
+      <div class="flex flex-col justify-between h-6 w-6 cursor-pointer">
+        <div class="w-full h-1 bg-gray-800"></div>
+        <div class="w-full h-1 bg-gray-800 my-1"></div>
+        <div class="w-full h-1 bg-gray-800"></div>
       </div>
     </header>
   );
