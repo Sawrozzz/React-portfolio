bb3975ac094bc166ef39bb2ee3d8e1e18682028a
[saroj adhikari] <sawroz.adhikari@gmail.com>
2024-03-21 09:49:59 +0545

    adding hover in nav

--- app/components/Header/page.js
+++ app/components/Header/page.js
@@ -1,8 +1,6 @@
 "use client";
 import React from "react";
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Page = () => {
   const [click, setClick] = useState(false);
   const hamburgerMenu = () => {
@@ -10,33 +8,33 @@ const Page = () => {
     console.log("Clicked");
   };
   return (
-    <header className="flex items-center justify-between bg-red-300 py-4 px-6">
-      <img className="h-12 w-13 mr-4" src="logo.png" alt="logo" />
+    <header className="flex items-center justify-between bg-green-400 py-4 px-6">
+      <img className="h-12 w-13 mr-4 cursor-pointer" src="logo.png" alt="logo" />
 
       <nav className="hidden sm:block items-end justify-end">
         <ul className="flex justify-end items-end">
           <li className="mr-4">
-            <a href="#" className="text-white hover:text-gray-300">
+            <a href="#" className=" text-black hover:text-white">
               Home
             </a>
           </li>
           <li className="mr-4">
-            <a href="#" className="text-white hover:text-gray-300">
+            <a href="#" className="text-black hover:text-white">
               Skills
             </a>
           </li>
           <li className="mr-4">
-            <a href="#" className="text-white hover:text-gray-300">
+            <a href="#" className="text-black hover:text-white">
               Education
             </a>
           </li>
           <li className="mr-4">
-            <a href="#" className="text-white hover:text-gray-300">
+            <a href="#" className="text-black hover:text-white">
               About
             </a>
           </li>
           <li>
-            <a href="#" className="text-white hover:text-gray-300">
+            <a href="#" className="text-black hover:text-white">
               Contact
             </a>
           </li>
