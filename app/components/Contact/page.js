8a22de86440881c4b4171ce58243b097c059c9dc
[saroj adhikari] <sawroz.adhikari@gmail.com>
2024-04-22 22:06:20 +0545

    added contact component

---
+++ app/components/Contact/page.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section className="bg-gray-200 py-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h2 className="text-3xl font-extrabold text-gray-900 text-center mb-8">Contact Me</h2>
+        <div className="max-w-lg mx-auto">
+          <form className="space-y-6" action='https://getform.io/f/pamqmnoa' method='POST' encType='multipart/form-data'>
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                Name
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                autoComplete="name"
+                required
+                className="mt-1 p-3 block w-full border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                Email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="mt-1 p-3 block w-full border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              />
+            </div>
+            <div>
+              <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                rows="4"
+                required
+                className="mt-1 p-3 block w-full border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+              ></textarea>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 disabled:opacity-50"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
