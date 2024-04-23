eb995e8a9c32264275ae898f07329407aad886bf
[saroj adhikari] <sawroz.adhikari@gmail.com>
2024-04-23 09:04:04 +0545

    added footer

---
+++ app/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-bg-2 text-white py-6 border-t-2 border-red-400">
+      <div className="container mx-auto flex items-center justify-center">
+        <p className="text-center">© 2024 Saroj Adhikari. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
