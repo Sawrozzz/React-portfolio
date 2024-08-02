163ea94d4b6dd1caf91ae5bdfbbd0cbe3c5c6629
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-08-02 09:17:18 +0545

    added web3form for email provider

--- app/components/Contact/page.js
+++ app/components/Contact/page.js
@@ -1,23 +1,49 @@
+"use client";
+
 import React from "react";
+import { useState } from "react";
 
 const Contact = () => {
+    const [result, setResult] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(e.target);
+
+    formData.append("access_key", "66ac8e3c-28cc-4676-84c1-a3b015f95e1d");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      e.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
-    <section className="bg-zinc-900 py-12">
+    <section id="contact" className=" bg-zinc-900 py-12">
       <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-extrabold text-blue-500 text-center mb-8 font-filroy">
           Contact Me
         </h2>
         <div className=" bg-transparent shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <form
-            className="space-y-6"
-            action="https://getform.io/f/pamqmnoa"
-            method="POST"
-            encType="multipart/form-data"
-          >
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <input
+              type="hidden"
+              name="access_key"
+              value="66ac8e3c-28cc-4676-84c1-a3b015f95e1d"
+            ></input>
             <div>
               <label
                 htmlFor="name"
-                className="bg-transparent block text-sm font-medium text-gray-700 font-filroy"
+                className="bg-transparent block text-sm font-medium text-gray-100 font-filroy"
               >
                 Name
               </label>
@@ -27,13 +53,13 @@ const Contact = () => {
                 type="text"
                 autoComplete="name"
                 required
-                className=" bg-transparent  font-filroy mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+                className=" bg-transparent  text-gray-100 font-filroy mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
               />
             </div>
             <div>
               <label
                 htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
+                className="block text-sm font-medium text-gray-100 font-filroy"
               >
                 Email
               </label>
@@ -43,13 +69,13 @@ const Contact = () => {
                 type="email"
                 autoComplete="email"
                 required
-                className=" bg-transparent  text-gray-700 font-filroy mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+                className=" bg-transparent  text-gray-100 font-filroy mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
               />
             </div>
             <div>
               <label
                 htmlFor="message"
-                className="block text-sm font-medium text-gray-700 font-filroy"
+                className="block text-sm font-medium text-gray-100 font-filroy"
               >
                 Message
               </label>
@@ -58,7 +84,7 @@ const Contact = () => {
                 name="message"
                 rows="4"
                 required
-                className="  bg-transparent font-filroy mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+                className="  bg-gray-500 font-filroy mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
               ></textarea>
             </div>
             <div>
@@ -72,6 +98,7 @@ const Contact = () => {
           </form>
         </div>
       </div>
+      <span className="text-2xl text-green-500 text-center">{result}</span>
     </section>
   );
 };
