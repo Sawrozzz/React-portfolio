253fc80881379d1cb37ea3a0f59b26db6f9595c7
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-08-03 18:09:57 +0545

    added new project

--- app/components/Project/page.js
+++ app/components/Project/page.js
@@ -27,16 +27,23 @@ const Project = () => {
       title: "Blog Website FullStack",
       image: "blog.png",
       description:
-        " This is secured and user friendly full stack blog website created using Node js. In this project I have implemented CURD operation uisng MongoDb and secured register and login system using hash and jwt tokens. ",
+        " This is secured and user friendly full stack blog website created using Node js. In this project I have implemented CRUD operation uisng MongoDb and secured register and login system using hash and jwt tokens. ",
       githubLink: "https://github.com/Sawrozzz/Blog",
+    },
+    {
+      title: "Redux Todo App",
+      image: "todo.png",
+      description:
+        "This is a user friendly todo app created using React and Redux. Here I have implemented CRUD operation using redux-toolkit.",
+      githubLink: "https://github.com/Sawrozzz/CODSOFT/tree/main/todo-list",
     },
     // Add more projects as needed
   ];
 
   return (
-    <section id='project' className="bg-zinc-900 py-12">
+    <section id="project" className="bg-zinc-900 py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-extrabold text-blue-500 text-center mb-8 font-filroy">
+        <h2 className="text-4xl font-extrabold text-blue-500 text-center mb-8 font-Lato">
           Projects
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
@@ -45,23 +52,23 @@ const Project = () => {
               key={index}
               className=" rounded-lg  p-6 hover:transform hover:scale-105 hover:bg-blue-200 transition duration-200 ease-in-out"
             >
-              <h3 className="text-xl text-orange-500 font-bold mb-4 font-filroy">
+              <h3 className="text-xl text-blue-500 font-bold mb-4 font-Ubuntu">
                 {project.title}
               </h3>
               <img
                 src={project.image}
                 alt={project.title}
-                className="w-full lg:w-2/2"
-              />{" "}
+                className="w-full lg:w-3/2 h-48 object-cover rounded"
+              />
               {/* Render the image */}
-              <p className=" text-gray-500 mb-4 font-filroy">
+              <p className=" text-gray-500 mt-2 mb-4 font-Ubuntu">
                 {project.description}
               </p>
               <a
                 href={project.githubLink}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                className="bg-blue-500 text-white font-Ubuntu py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
               >
                 GitHub Repository
               </a>
