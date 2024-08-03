d7e724b20299aff1ca28675b3627afdb5e9e84ac
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-08-03 18:07:56 +0545

    added new font

--- app/components/Contact/page.js
+++ app/components/Contact/page.js
@@ -31,7 +31,7 @@ const Contact = () => {
   return (
     <section id="contact" className=" bg-zinc-900 py-12">
       <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-extrabold text-blue-500 text-center mb-8 font-filroy">
+        <h2 className="text-3xl font-extrabold text-blue-500 text-center mb-8 font-Ubuntu">
           Contact Me
         </h2>
         <div className=" bg-transparent shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -44,7 +44,7 @@ const Contact = () => {
             <div>
               <label
                 htmlFor="name"
-                className="bg-transparent block text-sm font-medium text-gray-100 font-filroy"
+                className="bg-transparent block text-sm font-medium text-gray-100 font-Ubuntu"
               >
                 Name
               </label>
@@ -54,13 +54,13 @@ const Contact = () => {
                 type="text"
                 autoComplete="name"
                 required
-                className=" bg-transparent  text-gray-100 font-filroy mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+                className=" bg-transparent  text-gray-100 font-Ubuntu mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
               />
             </div>
             <div>
               <label
                 htmlFor="email"
-                className="block text-sm font-medium text-gray-100 font-filroy"
+                className="block text-sm font-medium text-gray-100 font-Ubuntu"
               >
                 Email
               </label>
@@ -70,13 +70,13 @@ const Contact = () => {
                 type="email"
                 autoComplete="email"
                 required
-                className=" bg-transparent  text-gray-100 font-filroy mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+                className=" bg-transparent  text-gray-100 font-Ubuntu mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
               />
             </div>
             <div>
               <label
                 htmlFor="message"
-                className="block text-sm font-medium text-gray-100 font-filroy"
+                className="block text-sm font-medium text-gray-100 font-Ubuntu"
               >
                 Message
               </label>
@@ -85,13 +85,13 @@ const Contact = () => {
                 name="message"
                 rows="4"
                 required
-                className="  bg-gray-500 font-filroy mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
+                className="  bg-gray-500 font-Ubuntu mt-1 p-3 block w-full border border-zinc-700 rounded-md shadow-sm focus:outline-none focus:border-indigo-500"
               ></textarea>
             </div>
             <div>
               <button
                 type="submit"
-                className="w-full font-filroy flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 disabled:opacity-50"
+                className="w-full font-Ubuntu flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 disabled:opacity-50"
               >
                 Submit
               </button>
@@ -99,7 +99,7 @@ const Contact = () => {
           </form>
         </div>
       </div>
-      <span className="text-2xl text-green-500 text-center">{result}</span>
+      <span className="text-2xl text-green-500 text-center font-Ubuntu">{result}</span>
     </section>
   );
 };
