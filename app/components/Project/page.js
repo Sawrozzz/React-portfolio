2186ff409b0181021178baa50bb1fb5722d03823
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-08-04 08:42:48 +0545

    redesign

--- app/components/Project/page.js
+++ app/components/Project/page.js
@@ -1,38 +1,39 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
 
 const Project = () => {
   const projects = [
     {
       title: "Login Interface",
-      image: "login.png",
+      image: "/login.png",
       description:
         "My first ever project in the field of web design where I try to design a login interface with the help of Html and CSS.",
       githubLink: "https://github.com/Sawrozzz/Login-Interface",
     },
     {
       title: "Real-Time Weather Forecast",
-      image: "weather.png",
+      image: "/weather.png",
       description:
         "This project gives the real time weather forecast, which use Api of OpenWeather. I have learned about fetching and asyncronous javascript with this project.",
       githubLink: "https://github.com/Sawrozzz/Weather-App",
     },
     {
       title: "Full Stack Sign-Up",
-      image: "auth.png",
+      image: "/auth.png",
       description:
         "This is a full stack project for a login and signup page. For this I use react and bootstrap for frontend and Node with Express for Backend and MonogDB as a Database",
       githubLink: "https://github.com/Sawrozzz/Auth_FullStack",
     },
     {
       title: "Blog Website FullStack",
-      image: "blog.png",
+      image: "/blog.png",
       description:
         " This is secured and user friendly full stack blog website created using Node js. In this project I have implemented CRUD operation uisng MongoDb and secured register and login system using hash and jwt tokens. ",
       githubLink: "https://github.com/Sawrozzz/Blog",
     },
     {
       title: "Redux Todo App",
-      image: "todo.png",
+      image: "/todo.png",
       description:
         "This is a user friendly todo app created using React and Redux. Here I have implemented CRUD operation using redux-toolkit.",
       githubLink: "https://github.com/Sawrozzz/CODSOFT/tree/main/todo-list",
@@ -41,34 +42,35 @@ const Project = () => {
   ];
 
   return (
-    <section id="project" className="bg-zinc-900 py-12">
+    <section id="project" className="pt-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-4xl font-extrabold text-blue-500 text-center mb-8 font-Lato">
+        <h2 className="text-4xl font-extrabold text-black  px-4 mb-8 font-Lato">
           Projects
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {projects.map((project, index) => (
             <div
               key={index}
-              className=" rounded-lg  p-6 hover:transform hover:scale-105 hover:bg-blue-200 transition duration-200 ease-in-out"
+              className=" rounded-full grid grid-row-4  p-6 hover:transform hover:scale-105 hover: transition duration-200 ease-in-out"
             >
-              <h3 className="text-xl text-blue-500 font-bold mb-4 font-Ubuntu">
+              <h3 className="text-xl text-black bg-bg-2 font-bold mb-4 font-Ubuntu">
                 {project.title}
               </h3>
-              <img
+              <Image
+                height={200}
+                width={400}
                 src={project.image}
                 alt={project.title}
-                className="w-full lg:w-3/2 h-48 object-cover rounded"
+                className="lg:w-3/2 h-48 object-cover rounded"
               />
-              {/* Render the image */}
-              <p className=" text-gray-500 mt-2 mb-4 font-Ubuntu">
+              <p className=" text-gray-600 mt-2 mb-4 font-Ubuntu">
                 {project.description}
               </p>
               <a
                 href={project.githubLink}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-blue-500 text-white font-Ubuntu py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                className="bg-black text-white font-Ubuntu py-2 px-4 rounded "
               >
                 GitHub Repository
               </a>
