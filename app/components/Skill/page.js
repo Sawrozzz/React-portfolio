711e66a9c7d4885ab176de0df27a01812f6f52b5
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-08-29 07:46:49 +0545

    add new skill

--- app/components/Skill/page.js
+++ app/components/Skill/page.js
@@ -14,88 +14,29 @@ import {
   ExpressJsLightIcon,
   MySQLIcon,
   MongoDBIcon,
+  MicrosoftSQLServerIcon,
 } from "developer-icons";
 
 const Skill = () => {
-  // const MySkills = [
-  //   {
-  //     image : <GitIcon />,
-  //     name: "Git",
-  //   },
-  //   {
-  //     image: <GitHubIcon />,
-  //     name: "GitHub",
-  //   },
-  //   {
-  //     image: <NodeJsIcon />,
-  //     name: "NodeJS",
-  //   },
-  //   {
-  //     image: <ExpressJsDarkIcon />,
-  //     name: "ExpressJS",
-  //   },
-  //   {
-  //     image: <MongoDBIcon />,
-  //     name: "MOngoDB",
-  //   },
-  //   {
-  //     image: <MySQLIcon />,
-  //     name: "SQLExpress",
-  //   },
-
-  //   {
-  //     image: <ReactIcon />,
-  //     name: "ReactJS",
-  //   },
-  //   {
-  //     image: <NextJsIcon />,
-  //     name: "NextJS",
-  //   },
-  //   {
-  //     image: <Bootstrap5Icon />,
-  //     name: "BootStraps",
-  //   },
-  //   {
-  //     image: <TailwindCSSIcon />,
-  //     name: "Tailwind CSS",
-  //   },
-  //   // {
-  //   //   image: "/c.png",
-  //   //   name: "C",
-  //   // },
-  //   {
-  //     image: <CPlusPlusIcon />,
-  //     name: "C++",
-  //   },
-  //   {
-  //     image: <HtmlIcon />,
-  //     name: "HTML",
-  //   },
-  //   {
-  //     image: <CSS3Icon />,
-  //     name: "CSS",
-  //   },
-  //   {
-  //     image: <JavascriptIcon />,
-  //     name: "JavaScript",
-  //   },
-  // ];
   return (
     <section id="skill" className="py-12 bg-stone-300">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-4xl font-extrabold text-black px-4 mb-8 font-Lato">
+        <h2 className="text-4xl text-center font-extrabold text-black px-4 mb-8 font-Lato">
           My Skills
         </h2>
-        <div className="flex gap-3 items-center justify-between">
-          <MongoDBIcon />
-          <NodeJsIcon />
-          <ExpressJsLightIcon />
-          <MySQLIcon />
-          <ReactIcon />
-          <NextJsIcon />
-          <Bootstrap5Icon />
-          <TailwindCSSIcon />
-          <CPlusPlusIcon />
+        <div className="flex gap-3 items-center justify-around flex-wrap ">
+          <MongoDBIcon className="cursor-pointer" />
+          <NodeJsIcon className="cursor-pointer" />
+          <ExpressJsLightIcon className="cursor-pointer" />
+          <MySQLIcon className="cursor-pointer " />
+          <ReactIcon className="cursor-pointer" />
+          <NextJsIcon className="cursor-pointer" />
+          <Bootstrap5Icon className="cursor-pointer" />
+          <TailwindCSSIcon className="cursor-pointer" />
+          <CPlusPlusIcon className="cursor-pointer" />
+          <GitIcon className="cursor-pointer" />
+          <GitHubIcon className="cursor-pointer" />
+          <CSS3Icon className="cursor-pointer" />
         </div>
       </div>
     </section>
