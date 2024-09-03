6a745cdad90a1191fce3259739d59fd8442fae77
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-09-03 18:38:57 +0545

    Feat: Added dark mode in skill page

--- app/components/Skill/page.js
+++ app/components/Skill/page.js
@@ -19,23 +19,23 @@ import {
 
 const Skill = () => {
   return (
-    <section id="skill" className="py-12 bg-stone-300">
+    <section id="skill" className="py-12 bg-stone-300 dark:bg-zinc-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-4xl text-center font-extrabold text-black px-4 mb-8 font-Lato">
+        <h2 className="text-4xl text-center font-extrabold text-black dark:text-white px-4 mb-8 font-Lato">
           My Skills
         </h2>
         <div className="flex gap-3 items-center justify-around flex-wrap ">
           <MongoDBIcon className="cursor-pointer" />
           <NodeJsIcon className="cursor-pointer" />
-          <ExpressJsLightIcon className="cursor-pointer" />
+          <ExpressJsLightIcon className="cursor-pointer dark:bg-white rounded-full" />
           <MySQLIcon className="cursor-pointer " />
           <ReactIcon className="cursor-pointer" />
-          <NextJsIcon className="cursor-pointer" />
+          <NextJsIcon className="cursor-pointer  dark:bg-white rounded-full " />
           <Bootstrap5Icon className="cursor-pointer" />
           <TailwindCSSIcon className="cursor-pointer" />
           <CPlusPlusIcon className="cursor-pointer" />
           <GitIcon className="cursor-pointer" />
-          <GitHubIcon className="cursor-pointer" />
+          <GitHubIcon className="cursor-pointer dark:bg-white rounded-full" />
           <CSS3Icon className="cursor-pointer" />
         </div>
       </div>
