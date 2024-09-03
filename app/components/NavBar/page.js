749eaaccffe9e62f5ad13bd5a810a9abfbf7bf63
Saroj Adhikari <sawroz.adhikari@gmail.com>
2024-09-03 18:38:09 +0545

    icons and logic for dark mode implemented complete

--- app/components/NavBar/page.js
+++ app/components/NavBar/page.js
@@ -2,41 +2,54 @@
 import React from "react";
 import Image from "next/image";
 import { Link } from "react-scroll";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { MdLightMode, MdOutlineDarkMode } from "react-icons/md";
 
 const NavBar = () => {
   const [navbar, setNavBar] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+  useEffect(() => {
+    const isDark = localStorage.getItem("darkmode") === "true";
+    setDarkMode(isDark);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("darkmode", darkMode);
+    document.body.classList.toggle("dark", darkMode);
+  }, [darkMode]);
+
   return (
     <>
-      <nav className="w-full bg-white  fixed top-0 left-0 right-0 z-10">
+      <nav className="w-full bg-white dark:bg-zinc-900   fixed top-0 left-0 right-0 z-10 ">
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
               {/*logo*/}
               <Link to="/" smooth={true} duration={500}>
-                <h3 className=" cursor-pointer text-black">Saroj</h3>
+                <h3 className=" cursor-pointer text-black dark:text-white">
+                  Saroj
+                </h3>
               </Link>
               <div className="md:hidden">
                 <button
-                  className="px-2 text-gray-700 rounded-md outline-none focus:border-grey-400"
+                  className="px-2 text-gray-700 dark:text-white rounded-md outline-none focus:border-grey-400"
                   onClick={() => setNavBar(!navbar)}
                 >
                   {navbar ? (
-                    <Image src="/cross.svg" width={30} height={30} alt="logo" />
+                    <Image src="/cross.svg" width={30} height={30} alt="logo" className="dark:bg-white rounded-full"/>
                   ) : (
                     <Image
                       src="/menu.svg"
                       width={30}
                       height={30}
                       alt="logo"
-                      className=" cursor-pointer focus:border-none active: border-none"
+
+                      className=" cursor-pointer focus:border-none active: border-none dark:bg-white"
                     />
                   )}
                 </button>
               </div>
             </div>
           </div>
-
           <div>
             <div
               className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
@@ -44,7 +57,7 @@ const NavBar = () => {
               }`}
             >
               <ul className=" h-screen md:h-auto items-center justify-center md:flex">
-                <li className=" font-Ubuntu cursor-pointer pd-6 text-xl text-black py-2 md:px-6 text-center  md:border-b-0hover:transform hover:scale-105 transition-transform   ">
+                <li className=" font-Ubuntu cursor-pointer pd-6 text-xl text-black dark:text-white py-2 md:px-6 text-center  md:border-b-0hover:transform hover:scale-105 transition-transform   ">
                   <Link
                     to="about"
                     smooth={true}
@@ -55,7 +68,7 @@ const NavBar = () => {
                     About
                   </Link>
                 </li>
-                <li className="pd-6 cursor-pointer font-Ubuntu text-xl text-black py-2 md:px-6 text-center md:border-b-0 hover:transform hover:scale-105 transition-transform   ">
+                <li className="pd-6 cursor-pointer font-Ubuntu text-xl text-black dark:text-white py-2 md:px-6 text-center md:border-b-0 hover:transform hover:scale-105 transition-transform   ">
                   <Link
                     to="project"
                     smooth={true}
@@ -65,7 +78,7 @@ const NavBar = () => {
                     Project
                   </Link>
                 </li>
-                <li className="pd-6 cursor-pointer font-Ubuntu text-xl text-black py-2 md:px-6 text-center  md:border-b-0hover:transform hover:scale-105 transition-transform   ">
+                <li className="pd-6 cursor-pointer font-Ubuntu text-xl text-black dark:text-white py-2 md:px-6 text-center  md:border-b-0hover:transform hover:scale-105 transition-transform   ">
                   <Link
                     to="skill"
                     smooth={true}
@@ -75,7 +88,7 @@ const NavBar = () => {
                     Skills
                   </Link>
                 </li>
-                <li className="pd-6  cursor-pointer font-Ubuntu text-xl text-black py-2 md:px-6 text-center  hover:transform hover:scale-105 transition-transform   ">
+                <li className="pd-6  cursor-pointer font-Ubuntu text-xl text-black dark:text-white py-2 md:px-6 text-center  hover:transform hover:scale-105 transition-transform   ">
                   <Link
                     to="contact"
                     smooth={true}
@@ -88,6 +101,21 @@ const NavBar = () => {
               </ul>
             </div>
           </div>
+        {
+          !darkMode ? (
+            <MdOutlineDarkMode
+            size={30}
+            onClick={()=>setDarkMode(!darkMode)}
+            />
+          ):
+          (
+            <MdLightMode
+            size={30}
+            className="  dark:text-white "
+            onClick={(()=>setDarkMode(!darkMode))}
+            />
+          )
+        }
         </div>
       </nav>
     </>
